f08c19f600bc2ce48de5e9ab0fa4d9c3a0a10517
M International Commerce <minternationalcommerce@gmail.com>
2025-06-09 15:57:36 +0000

    The Netlify deploy errored, with the following guidance provided:
    ### D

--- src/app/videos/page.tsx
+++
@@ -1,5 +0,0 @@
-
-// This file is no longer needed as its functionality has been merged into src/app/page.tsx
-// You can safely delete this file from your project.
-// If you have any links pointing to /videos, they should be updated or removed.
-// The "View All Videos" button on the homepage now triggers an inline video feed.
