bb88f69082022c3038743a612fa315cffc8ec29b
M International Commerce <minternationalcommerce@gmail.com>
2025-06-09 10:37:02 +0000

    in header, Yellow Institute text needs to be retina ready color

--- src/components/LuminaLogo.tsx
+++ src/components/LuminaLogo.tsx
@@ -3,7 +3,7 @@ import { BookOpenText } from 'lucide-react';
 
 const LuminaLogo = () => {
   return (
-    <Link href="/" className="flex items-center space-x-2 text-xl font-bold font-headline text-primary hover:text-primary/80 transition-colors">
+    <Link href="/" className="flex items-center space-x-2 text-xl font-bold font-headline text-primary hover:text-primary-darker transition-colors">
       <BookOpenText size={26} />
       <span>Yellow Institute</span>
     </Link>
