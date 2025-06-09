787c40b7d755a755973f768e882a23b9c7d49a4b
M International Commerce <minternationalcommerce@gmail.com>
2025-06-09 10:38:32 +0000

    logo text color change to colorful 3d

--- src/components/LuminaLogo.tsx
+++ src/components/LuminaLogo.tsx
@@ -1,11 +1,14 @@
+
 import Link from 'next/link';
 import { BookOpenText } from 'lucide-react';
 
 const LuminaLogo = () => {
   return (
     <Link href="/" className="flex items-center space-x-2 text-xl font-bold font-headline text-primary hover:text-primary-darker transition-colors">
-      <BookOpenText size={26} />
-      <span>Yellow Institute</span>
+      <BookOpenText size={24} /> {/* Adjusted size slightly to balance with potentially bolder text */}
+      <span style={{ textShadow: '1px 1px 0px hsl(var(--primary-darker)), 2px 2px 0px hsl(var(--accent-darker))' }}>
+        Yellow Institute
+      </span>
     </Link>
   );
 };
