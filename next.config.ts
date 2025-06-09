b3e67bb1a3d11110f6e7058677f542d9d334c9e7
M International Commerce <minternationalcommerce@gmail.com>
2025-06-09 15:16:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'monitordata.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
