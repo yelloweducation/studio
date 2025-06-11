3d3bb86a9190b728be2b656c4319b4bb167bac6f
M International Commerce <minternationalcommerce@gmail.com>
2025-06-11 07:18:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/lib/prisma.ts
@@ -0,0 +1,20 @@
+
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-unused-vars
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    // log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prisma;
